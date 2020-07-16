72f88d144e2126016829f35a0466fd20d28b8770
Agostinho <agostinhojr@gmail.com>
2020-07-16 13:30:51 -0300

    Versão Teste 1.0

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public int TotalScore;
+    
+    public static GameController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+}
